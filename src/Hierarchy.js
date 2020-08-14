e88852f60eca8c0b3259701f370cc0a2d1d3457d
Jeet S <clutchcityred@gmail.com>
2020-08-14 16:26:29 -0500

    switch hierarchies; scrub hierarchy2 data

--- src/Hierarchy.js
+++ src/Hierarchy.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import {
+  Select, MenuItem
+} from '@material-ui/core'
 import SortableTree from 'react-sortable-tree';
 import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 import hierarchy from './data/hierarchy.json';
+import hierarchy2 from './data/hierarchy2.json';
 
 function treeify(list, idAttr, parentAttr, childrenAttr) {
   if (!idAttr) idAttr = 'id';
@@ -24,19 +28,45 @@ function treeify(list, idAttr, parentAttr, childrenAttr) {
   return treeList;
 };
 
+const hierarchies = [hierarchy, hierarchy2];
+
 export default function Hierarchy() {
+  const [selectedHierarchy, setSelectedHierarchy] = useState([]);
   const [treeData, setTreeData] = useState([]);
 
   useEffect(() => {
-    setTreeData(treeify(hierarchy.Relationship, 'title', 'parent', 'children'));
+    setSelectedHierarchy(hierarchies[0]);
+    setTreeData(treeify(hierarchies[0].Relationship, 'title', 'parent', 'children'));
   }, []);
+
+  const handleHierarchyChange = (event) => {
+    let newSelectedHierarchy = event.target.value;
+    setSelectedHierarchy(newSelectedHierarchy);
+    setTreeData(treeify(newSelectedHierarchy.Relationship, 'title', 'parent', 'children'));
+  };
 
   return (
     <div style={{ height: 1000 }}>
+      <Select
+        labelId="entity-type-select-label"
+        id="entity-type-select"
+        value={selectedHierarchy}
+        onChange={handleHierarchyChange}
+      >
+        {hierarchies.map((hierarchy, index) => (
+          <MenuItem
+            key={index}
+            className="hierarchyMenuItem"
+            value={hierarchy}
+          >
+            {hierarchy.Name}
+          </MenuItem>
+        ))}
+      </Select>
       <SortableTree
         treeData={treeData}
         onChange={treeData => setTreeData(treeData)}
-        style={{color:"black"}}
+        style={{ color: "black" }}
       />
     </div>
   );

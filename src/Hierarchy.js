bae988bfc97c572b49c6d898d9c4f92f39c09907
Jeet S <clutchcityred@gmail.com>
2020-08-14 16:54:57 -0500

    cleanup

--- src/Hierarchy.js
+++ src/Hierarchy.js
@@ -100,16 +100,6 @@ export default function Hierarchy() {
           event.preventDefault();
         }}
       >
-        {/* <input
-          id="find-box"
-          type="text"
-          placeholder="Search..."
-          style={{ fontSize: '1rem' }}
-          value={searchString}
-          onChange={event =>
-            this.setState({ searchString: event.target.value })
-          }
-        /> */}
 
         <button
           type="button"
@@ -138,33 +128,22 @@ export default function Hierarchy() {
       <SortableTree
         treeData={treeData}
         onChange={treeData => setTreeData(treeData)}
-        
-            //
-            // Custom comparison for matching during search.
-            // This is optional, and defaults to a case sensitive search of
-            // the title and subtitle values.
-            // see `defaultSearchMethod` in https://github.com/frontend-collective/react-sortable-tree/blob/master/src/utils/default-handlers.js
-            // searchMethod={customSearchMethod}
-            //
-            // The query string used in the search. This is required for searching.
-            searchQuery={searchQuery}
-            //
-            // When matches are found, this property lets you highlight a specific
-            // match and scroll to it. This is optional.
-            searchFocusOffset={searchFocusIndex}
-            //
-            // This callback returns the matches from the search,
-            // including their `node`s, `treeIndex`es, and `path`s
-            // Here I just use it to note how many matches were found.
-            // This is optional, but without it, the only thing searches
-            // do natively is outline the matching nodes.
-            searchFinishCallback={matches =>
-              searchFinishCallback(matches)
-            }
-            //
-            // This prop only expands the nodes that are seached.
-            onlyExpandSearchedNodes
+        // The query string used in the search. This is required for searching.
         searchQuery={searchQuery}
+        //
+        // When matches are found, this property lets you highlight a specific
+        // match and scroll to it. This is optional.
+        searchFocusOffset={searchFocusIndex}
+        //
+        // This callback returns the matches from the search,
+        // including their `node`s, `treeIndex`es, and `path`s
+        // Here I just use it to note how many matches were found.
+        // This is optional, but without it, the only thing searches
+        // do natively is outline the matching nodes.
+        searchFinishCallback={matches => searchFinishCallback(matches)}
+        //
+        // This prop only expands the nodes that are seached.
+        onlyExpandSearchedNodes
         style={{ color: "black" }}
       />
     </div>

48b205a1da59ae6493c81046cd6cdf4aec806f0c
Jeet S <clutchcityred@gmail.com>
2020-08-16 10:15:40 -0500

    view data in table

--- src/views/EntitiesTable.js
+++ src/views/EntitiesTable.js
@@ -1,14 +1,70 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useTable } from 'react-table'
 
 import hierarchy from '../data/hierarchy.json';
 import hierarchy2 from '../data/hierarchy2.json';
 
 export default function EntitiesTable({ selectedEntity, entities }) {
+  const getColumnConfig = () => {
+    let columnConfig = [
+      { Header: 'Name', accessor: 'Name' },
+      { Header: 'Entity Type', accessor: 'EntityTypeName' },
+      { Header: 'Alias', accessor: 'Alias' }
+    ];
+    return columnConfig;
+  }
 
   return (
     <div>
       <h1>Entities Table</h1>
-      <h2>{ selectedEntity.Name }</h2>
+      <h2>{selectedEntity.Name}</h2>
+      <Table
+        // minRows={1}
+        columns={getColumnConfig()}
+        data={entities}
+      //  className="-striped -highlight" 
+      />
     </div>
   );
+}
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }

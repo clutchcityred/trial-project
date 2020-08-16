da1f50fdbcf3b1352d7107b3198c6dd408462b20
Jeet S <clutchcityred@gmail.com>
2020-08-16 10:21:18 -0500

    style table

--- src/views/EntitiesTable.js
+++ src/views/EntitiesTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTable } from 'react-table'
+import styled from 'styled-components'
 
 import hierarchy from '../data/hierarchy.json';
 import hierarchy2 from '../data/hierarchy2.json';
@@ -18,12 +19,14 @@ export default function EntitiesTable({ selectedEntity, entities }) {
     <div>
       <h1>Entities Table</h1>
       <h2>{selectedEntity.Name}</h2>
-      <Table
-        // minRows={1}
-        columns={getColumnConfig()}
-        data={entities}
-      //  className="-striped -highlight" 
-      />
+      <Styles>
+        <Table
+          // minRows={1}
+          columns={getColumnConfig()}
+          data={entities}
+        //  className="-striped -highlight" 
+        />
+      </Styles>
     </div>
   );
 }
@@ -68,3 +71,32 @@ function Table({ columns, data }) {
     </table>
   )
 }
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
